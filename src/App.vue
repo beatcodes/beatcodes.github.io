a52fd5faa6c4d02402be6c1f9b71738e7132f811
linking <328043646@qq.com>
2022-03-15 19:51:27 +0800

    新建一个vite-vue3空项目

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,13 @@
-<template>
-  <div id="app">
-    <router-view />
-  </div>
-</template>
-
-<script>
-export default {
-  name: "App",
-};
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import HelloWorld from './components/HelloWorld.vue'
 </script>
+
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3 + Vite" />
+</template>
 
 <style>
 #app {
--- src/views/Mine.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div>mine</div>
-</template>
-
-<script>
-export default {};
-</script>
-
-<style></style>
